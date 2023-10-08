d8c7bb64f5b53a9d2b8d65764fb19c47184c7ec1
haikali3 <105930061+haikali3@users.noreply.github.com>
2023-10-09 00:22:07 +0800

    profile card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
 import CortanaImage from './images/cortana.png';
 import AlexaImage from './images/alexa.png';
@@ -11,11 +10,42 @@ console.log(SiriImage);
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
 
-      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
-      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
-      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Alexa was created by Amazon"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+                description="Cortana was made by Microsoft"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description="Siri was made by Apple and is being phased out"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
